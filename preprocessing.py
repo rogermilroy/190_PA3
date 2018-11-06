cc1e29c8d6c5e2d6805231bd636691dd2bd92990
rogermilroy <rogermilroy@icloud.com>
2018-11-06 15:16:42 -0800

    Remove multithreading.

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,12 @@ import os
 from pathlib import Path
 import concurrent.futures
 
+
+def save_image(int, data):
+    image_label = data[int]
+    torch.save(image_label, str(Path.home()) + '/processed/image-' + str(i))
+
+
 if not os.path.exists(str(Path.home()) + '/processed'):
     os.makedirs(str(Path.home()) + '/processed')
 
@@ -12,14 +18,8 @@ transform = transforms.Compose([transforms.Resize(512), transforms.ToTensor()])
 
 dataset = xray_dataloader.ChestXrayDataset(transform=transform)
 
-def save_image(int, data):
-    image_label = data[int]
-    torch.save(image_label, str(Path.home()) + '/processed/image-' + str(i))
-
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
-    for i in range(len(dataset)):
-        executor.submit(save_image, i, dataset)
+for i in range(len(dataset)):
+    save_image(i, dataset)
 
 
 
