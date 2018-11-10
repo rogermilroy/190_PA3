Fix weighting.

k_fold_basic_weighted.py
@@ -44,8 +44,12 @@ total_samples = 112120.0
 
 frequencies = torch.tensor([11535.0, 2772.0, 13307.0, 19870.0, 5746.0, 6323.0, 1353.0, 5298.0 ,
                             4667.0, 2303.0, 2516.0, 1686.0, 3385.0, 227.0])
+samples = torch.zeros_like(frequencies)
+samples += total_samples
 
-weights = frequencies / total_samples
+weights = total_samples / frequencies
+print(weights)
+
 
 # Use bce with logits for additional numerical stability.
 criterion = torch.nn.BCEWithLogitsLoss(weight=weights.to(computing_device)).to(computing_device)