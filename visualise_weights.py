c3284e510b2256a0f1208f2d3fffb5512184cb07
Anh Ngo <adn057@ucsd.edu>
2018-11-12 20:27:18 -0800

    add visualization files

--- visualise_weights.py
+++ visualise_weights.py
@@ -2,15 +2,24 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 from deep_and_small import DeepAndSmallCNN
-from pathlib import Path
+from baseline_cnn import BasicCNN
+import testing
+import os
 
-image = torch.load(str(Path.home()) + '/image-20')
+image = torch.load(os.getcwd() + '/images/image-20')
 
 model = DeepAndSmallCNN()
 
-weights = torch.load('./results/kfold-deep-small/test-0-params')
+#weights = torch.load(os.getcwd() + '/results/kfold-deep-small/test-0-params')
 
-model.load_state_dict(weights)
+#model.load_state_dict(weights)
 
-output = model(image)
+#output = model(image[0])
+img = image[0]
+plt.gray()
+plt.imshow(img[0])
+print(img[0].shape)
+plt.imshow(img[0])
 
+print(image)
+#print(output)
